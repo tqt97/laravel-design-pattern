6d2aa235ee717a0db7fca070c5e09738f1c6b118
QuocTuanIT <kutuanonline199@gmail.com>
2024-09-10 14:42:29 +0700

    add model - migration - facfory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +16,12 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        Product::factory(50)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'TuanTQ',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('12341234')
         ]);
     }
 }
